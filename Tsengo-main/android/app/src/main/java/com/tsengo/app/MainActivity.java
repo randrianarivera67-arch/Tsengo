ff25d35248eac3dff2ba640f27d5eec65a040a24
randrianarivera67-arch <randrianarivera67@gmail.com>
2026-07-08 13:48:18 +0300

    clean: suppression dossier Tsengo-main dupliqué

--- Tsengo-main/android/app/src/main/java/com/tsengo/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.tsengo.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- Tsengo-main/src/utils/chat.js
+++
@@ -1,4 +0,0 @@
-// src/utils/chat.js
-export function getChatId(uid1, uid2) {
-  return [uid1, uid2].sort().join('_');
-}
